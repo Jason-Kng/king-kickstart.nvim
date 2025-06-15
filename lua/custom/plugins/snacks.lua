b68c2002b898d011eea1cfbcca71a66d3a37b3d6
Jason King <jek632005@gmail.com>
2025-06-15 06:06:00 -0500

    Added a dashboard plugin

---
+++ lua/custom/plugins/snacks.lua
@@ -0,0 +1,11 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    dashboard = { enabled = true },
+  },
+}
+
+--vim: ts=2 sts=2 sw=2 et
